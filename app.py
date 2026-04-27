22b85d53f20c6ff906518ebc818e07c3a6533a02
Yuri Kotik <4520755@gmail.com>
2026-04-27 18:33:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+from supabase import create_client
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+url = "https://asnyvuksnlhvqajrtrrr.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzbnl2dWtzbmxodnFhanJ0cnJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMDI4MzcsImV4cCI6MjA5Mjg3ODgzN30.xH4yGSJssOfas5_G4-0G3lrFYZ57G4biTvguquzp250"
+
+supabase = create_client(url, key)
+
